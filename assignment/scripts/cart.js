84835a9034ed3456d2a6d6ee49a08cad83eaf2e5
Terry O'Keefe <terry.okeefe87@gmail.com>
2021-04-23 17:51:25 -0500

    added items to addItem

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,37 +2,48 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+console.log( '1.) - Create addItem Function' );
+
 let basket = [];
 
 function addItem ( item ){ // add items to basket
-  basket.push(item);
+  basket.push(item); // add item to basket
   console.log( 'Added item:', item );
-  return true;
+  return true; // return true if item is added to basket
 }
 
-console.log( `Basket is${basket}`);
-console.log('Add apple to list:', addItem('apple'));
-console.log( 'Add banana to list:', addItem('banana'));
-console.log( `Basket is now ${basket}`);
+console.log( `Basket is ${basket}` ); // check to see basket is empty
+console.log( 'Add apple to list:', addItem( 'apple' ) ); // add apple to function
+console.log( 'Add banana to list:', addItem( 'banana' ) ); // add banana to function
+console.log( 'Add bacon to list:', addItem( 'bacon' ) ); // add bacon to function
+console.log( `Basket is now ${basket}` ); // check to see that items run through function and added to basket
+
+console.log( '2.) - Create listItems Function' );
 
 function listItems ( array ){ // list items in basket
   for ( let i = 0; i < array.length; i++ ) {
-    if ( array < array.length ) {
-       console.log( array[i] );
+       console.log( 'added to ListItems:', array[i] );
     }
-  }
-      console.log( 'Nothing in Basket');
+    return array;
 }
 
-
-console.log( basket );
-console.log( 'Items in my basket', basket.length);
+//console.log( basket );
+//console.log( 'Items in my basket', basket.length);
 console.log( 'What items are in my basket?', listItems( basket ));
+//console.log(listItems(basket));
 
 function empty ( reset ){
   basket.length = 0; // remove all items in cart
-  return console.log( 'Empty' );
+  return true;
 }
 
-console.log( `Basket is ${basket}`);
-console.log( 'Basket is', empty(basket));
+//console.log( `Basket is ${basket}`);
+//console.log( 'Basket is', empty(basket));
+//console.log( '');
+
+console.log('TEST');
+
+let tree = [ 'pine', 'spruce' ];
+for (let i = 0; i < tree.length; i++) {
+  console.log(tree[i]);
+}
