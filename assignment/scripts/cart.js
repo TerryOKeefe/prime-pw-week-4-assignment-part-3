998a61ab34b60570c03e87eb560c49e2a3a6f6b3
Terry O'Keefe <terry.okeefe87@gmail.com>
2021-04-23 18:26:44 -0500

    main assignment done w/ comments

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,46 +4,53 @@ console.log('***** Cart Functions *****');
 
 console.log( '1.) - Create addItem Function' );
 
+//create variable named basket and set to an empty array
 let basket = [];
 
+// create addItem function
 function addItem ( item ){ // add items to basket
   basket.push(item); // add item to basket
   console.log( 'Added item:', item );
   return true; // return true if item is added to basket
 }
 
-console.log( `Basket is ${basket}` ); // check to see basket is empty
+// console.log to test addItem function
+console.log( 'Basket is:', basket ); // check to see basket is empty
 console.log( 'Add apple to list:', addItem( 'apple' ) ); // add apple to function
 console.log( 'Add banana to list:', addItem( 'banana' ) ); // add banana to function
 console.log( 'Add bacon to list:', addItem( 'bacon' ) ); // add bacon to function
 console.log( `Basket is now ${basket}` ); // check to see that items run through function and added to basket
 
+// Function to list items in array
 console.log( '2.) - Create listItems Function' );
 
 function listItems ( array ){ // list items in basket
-  for ( let i = 0; i < array.length; i++ ) {
+  for ( let i = 0; i < array.length; i++ ) { // start of for loop to run through array
        console.log( 'added to ListItems:', array[i] );
     }
     return array;
-}
+} // end listItems function
 
-//console.log( basket );
-//console.log( 'Items in my basket', basket.length);
+// console.log to test functions
+console.log( basket );
+console.log( 'Items in my basket', basket.length);
 console.log( 'What items are in my basket?', listItems( basket ));
-//console.log(listItems(basket));
 
-function empty ( reset ){
-  basket.length = 0; // remove all items in cart
+// Extra practice running functions
+console.log( '2a.) - Add milk and run functions');
+console.log( 'Add milk to list:', addItem( 'milk' ) );
+console.log( 'Items now in my basket:', listItems( basket ));
+
+// Function to empty array
+console.log( '3.) - Create empty Function' );
+
+function empty ( reset ){ // start function to empty array
+  reset.length = 0; // set array length 0
   return true;
-}
+} // end empty functions
 
-//console.log( `Basket is ${basket}`);
-//console.log( 'Basket is', empty(basket));
+// console.log to test empty function
+console.log( `Basket is ${basket}`);
+console.log( `Empty Basket: ${empty(basket)}`);
+console.log( `Basket is now ${basket}` );
 //console.log( '');
-
-console.log('TEST');
-
-let tree = [ 'pine', 'spruce' ];
-for (let i = 0; i < tree.length; i++) {
-  console.log(tree[i]);
-}
