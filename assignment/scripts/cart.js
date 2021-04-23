89b7503de24572a6f50a475520a027c0725be191
Terry O'Keefe <terry.okeefe87@gmail.com>
2021-04-22 19:00:08 -0500

    basic layout-needs more work

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -6,23 +6,33 @@ let basket = [];
 
 function addItem ( item ){ // add items to basket
   basket.push(item);
+  console.log( 'Added item:', item );
   return true;
 }
 
+console.log( `Basket is${basket}`);
 console.log('Add apple to list:', addItem('apple'));
+console.log( 'Add banana to list:', addItem('banana'));
+console.log( `Basket is now ${basket}`);
 
 function listItems ( array ){ // list items in basket
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < array.length) {
-      console.log(array[i]);
+  for ( let i = 0; i < array.length; i++ ) {
+    if ( array < array.length ) {
+       console.log( array[i] );
     }
   }
-  return array;
+      console.log( 'Nothing in Basket');
 }
 
-console.log('What items are in my basket?', listItems());
+
+console.log( basket );
+console.log( 'Items in my basket', basket.length);
+console.log( 'What items are in my basket?', listItems( basket ));
 
 function empty ( reset ){
-
-  return; // remove all items in cart
+  basket.length = 0; // remove all items in cart
+  return console.log( 'Empty' );
 }
+
+console.log( `Basket is ${basket}`);
+console.log( 'Basket is', empty(basket));
